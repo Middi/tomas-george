060f58e2a4543a1aa9980d7b3170a334678108bb
“Richard Middleton” <Richard@richardmiddleton.me>
2017-12-03 01:54:11 +0000

    hero image, cta, about and started cards

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
-$(document).ready(function() {
-    
-    $("#navToggle a").click(function(e) {
-      e.preventDefault();
-      $("nav").slideToggle("medium");
+$(document).ready(function () {
+
+    $("#navToggle a").click(function (e) {
+        e.preventDefault();
+        $("nav").slideToggle("medium");
     });
-  
-    $(window).resize(function() {
-      if ($(window).width() >= "720") {
-        $("nav").css("display", "block");
-      }
-      else {
-        $("nav").css("display", "none");
-      }
+
+    $(window).resize(function () {
+        if ($(window).width() >= "720") {
+            $("nav").css("display", "block");
+        } else {
+            $("nav").css("display", "none");
+        }
     });
-  
-  });
-  
-  
+
+
+
+    // ===========================
+    // Smooth Scrolling
+    // ===========================
+
+    $('body').on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        var target_offset = $(this.hash).offset() ? $(this.hash).offset().top : 0;
+        //change this number to create the additional off set        
+        var customoffset = 0
+        $('html, body').animate({
+            scrollTop: target_offset - customoffset
+        }, 1000);
+    });
+
+});
